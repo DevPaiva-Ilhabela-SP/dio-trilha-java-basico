e9fd30e8bb67671fbfff1111f94aa908c90046b6
Rodrigo de Paiva Costa <rodrigo_paiva_costa@hotmail.com>
2025-03-12 16:43:48 -0300

    projeto conta bancaria em funcionamento

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner; 
+// TODO: conhecer e importar a classe Scanner
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // Criar um objeto Scanner para obter entrada do usuário
+        Scanner scanner = new Scanner(System.in);
+
+        // Exibir mensagens para o usuário e obter os valores digitados
+        System.out.println("Bem-vindo ao sistema bancário GreenCamp!");
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite o número da conta com 5 dígitos : ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.print("Digite o saldo inicial: ");
+        double saldo = scanner.nextDouble();
+
+        // Fechar o scanner
+        scanner.close();
+
+        // Exibir a mensagem de conta criada
+        System.out.println("Conta criada com sucesso!");
+        System.out.println("Titular: " + nome);
+        System.out.println("Número da Conta: " + numeroConta);
+        System.out.println("Saldo Inicial: R$" + saldo);
     }
 }
+
